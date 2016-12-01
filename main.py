004e769a44dbb7c85c77f3bcb46bc5c34e0cf64f
charlievweiss <charlotte.weiss@students.olin.edu>
2016-11-30 21:02:36 -0500

    Trying to get brennan and arpan's code to integrate. check main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """Skateboard web app developed using MVC framework (eventually compatible with WSGI for deployment)"""
 
 from flask import Flask,render_template,request
-#import board_math
+import board_math
 import hexdraw
 
 app = Flask(__name__)
@@ -19,15 +19,16 @@ def index():
 def login():
 	rider_weight=request.form['rider_weight']
 	board_length=request.form['board_length']
-	riding_style=request.form['riding_style'] 
+	riding_style=request.form['riding_style']
 	if rider_weight and riding_style:
 		# Call board_math.py to get board parameters
-		# Returns list of side_length,fiverglass_layers,web_thickness
-		#Outputs = board_math.board_math(rider_weight,riding_style)
-		#print Outputs
+		# Returns list of small circle diameter, big circle diameter
+		riding_style = int(riding_style) #necessary to pass to board_math
+		Outputs = board_math.board_math(rider_weight,riding_style)
 		# TODO: next 2 lines as things in Outputs
-		diameter1 = 2
-		diameter2 = 3
+		diameter1 = Outputs[0]
+		diameter2 = Outputs[1]
+		print diameter1,diameter2
 		# Call hexdraw.py to generate DXF
 		filename = hexdraw.hexdraw(diameter1,diameter2)
 		# TODO: Convert filename DXF to an actually presentable thing.
@@ -39,7 +40,7 @@ def login():
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
-    return render_template('hello.html', name=name)
+	return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
 	app.run()
