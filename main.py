66959006f85c5b3454f296516f95ff9ffcaa3109
charlievweiss <charlotte.weiss@students.olin.edu>
2016-12-01 05:43:50 -0500

    currently directs to profile_page. Still troubleshooting integration with board_math

--- main.py
+++ main.py
@@ -18,22 +18,22 @@ def index():
 @app.route('/login',methods=['POST'])
 def login():
 	rider_weight=request.form['rider_weight']
-	board_length=request.form['board_length']
 	riding_style=request.form['riding_style']
 	if rider_weight and riding_style:
 		# Call board_math.py to get board parameters
 		# Returns list of small circle diameter, big circle diameter
 		riding_style = int(riding_style) #necessary to pass to board_math
-		Outputs = board_math.board_math(rider_weight,riding_style)
+		#Outputs = board_math.board_math(rider_weight,riding_style)
 		# TODO: next 2 lines as things in Outputs
-		diameter1 = Outputs[0]
-		diameter2 = Outputs[1]
-		print diameter1,diameter2
+		#diameter1 = Outputs[0]
+		#diameter2 = Outputs[1]
+		#print diameter1,diameter2
 		# Call hexdraw.py to generate DXF
-		filename = hexdraw.hexdraw(diameter1,diameter2)
+		#filename = hexdraw.hexdraw(diameter1,diameter2)
 		# TODO: Convert filename DXF to an actually presentable thing.
 		# the return statement should be Andrew's code
-		return render_template('downloadpage.html', filename=filename)
+		# return render_template('downloadpage.html', filename=filename)
+		return render_template('profile_page.html',weight=rider_weight,style=riding_style)
 	else:
 		return render_template('error_page.html')
 
