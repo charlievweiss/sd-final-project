bedeaf307c98a02c936b1955ebec9cd99caa0709
charlievweiss <charlotte.weiss@students.olin.edu>
2016-12-01 06:14:58 -0500

    All parts of code now integrated

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 """Skateboard web app developed using MVC framework (eventually compatible with WSGI for deployment)"""
 
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request, redirect, url_for, send_from_directory
 import board_math
 import hexdraw
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = '/home/charlie/softdes/sd-final-project'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'DXF'])
 
 app = Flask(__name__)
-
-# All this is part of the Viewer
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 # main page - accepts user riding preferences
 @app.route('/')
@@ -22,24 +26,23 @@ def login():
 	if rider_weight and riding_style:
 		# Call board_math.py to get board parameters
 		# Returns list of small circle diameter, big circle diameter
-		rider_weight = int(rider_weight) #check if can make original inputs ints
-		riding_style = int(riding_style) #necessary to pass to board_math
+		rider_weight = int(rider_weight) #TODO: check if can make original inputs ints
+		riding_style = int(riding_style)
 		Outputs = board_math.board_math(rider_weight,riding_style)
 		diameter1 = Outputs[0]
 		diameter2 = Outputs[1]
 		# Call hexdraw.py to generate DXF
 		filename = hexdraw.hexdraw(diameter1,diameter2)
 		# TODO: Convert filename DXF to an actually presentable thing.
-		# the return statement should be Andrew's code
-		# return render_template('downloadpage.html', filename=filename)
-		return render_template('profile_page.html',weight=rider_weight,style=riding_style)
+		filename = 'http://127.0.0.1:5000/uploads/' + filename
+		return render_template('downloadpage.html', filename=filename)
 	else:
 		return render_template('error_page.html')
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-	return render_template('hello.html', name=name)
+# from Andrew
+@app.route('/uploads/<filename>')
+def send_file(filename):
+    return send_from_directory(UPLOAD_FOLDER, filename)
 
 if __name__ == '__main__':
 	app.run()
