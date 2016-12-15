e94492b45c5633930def2c37d0c583eac084128b
arpanrau <arpan.rau@gmail.com>
2016-12-15 18:45:55 -0500

    Got skateboard creation working

--- hexdraw.py
+++ hexdraw.py
@@ -1,6 +1,4 @@
-"""
-SolidpythonProgram that draws a hexagon of given dimensions
-"""
+
 from solid import *
 from solid.utils import *
 import os, sys
@@ -9,22 +7,37 @@ import math
 
 
 def hexdraw(insize,outsize):
-        SEGMENTS = 400
-        #outsize is tip to tip distance of outer hexagon
-        flatflat = outsize*math.sqrt(3)/2.0 #outsize * sqrt3/2 = flat to flat distance
-        out_dir = sys.argv[1] if len(sys.argv) > 1 else os.curdir
+        """Given a tip-to-tip size of inner and outer hexagons(insize and outsize respectively)
+        generates .DXF file of skateboard and returns path to .DXF
+        """
+        SEGMENTS = 400 #Resolution setting for OpenScad
+        flatflat = outsize*math.sqrt(3)/2.0 # outsize is tip to tip distance of outer hexagon, outsize * sqrt3/2 = flat to flat distance
+        print flatflat
+        #sets path out for generated OpenScad file to temp.scad in current directory
+        os.chdir('static')
+        out_dir = os.getcwd()
         file_out = os.path.join(out_dir, 'temp.scad')
-        #basic_geometry = import_('Core_Export_New.dxf')
-        basic_geometry = circle(3000)
-        for i in range(0,30):
+        core = import_('Core.dxf') #Import core 
+        #Draw one grid of hexagons
+        for i in range(-10,10):
                 #go right then draw another column
-                leftdist =flatflat*i*.75
-                for i in range(0,30):
-                        Hexagon1 = left(leftdist)(forward(outsize*i*.5)(circle(insize)))
+                leftdist=outsize*1.5*i+outsize*.75
+                for i in range(-10,10):
+                        Hexagon2 = left(leftdist)(forward((flatflat*i+flatflat/2))(circle(insize/2)))
+                        Hexagon2.add_param('$fn',6) # make hexagonal
+                        core = difference()(core, Hexagon2)
+        #Draw half offset grid to make hexmesh
+        for i in range(-10,10):
+                #go right then draw another column
+                leftdist=outsize*1.5*i
+                for i in range(-10,10):
+                        Hexagon1 = left(leftdist)(forward(flatflat*i)(circle(insize/2)))
                         Hexagon1.add_param('$fn',6) # make hexagonal
-                        basic_geometry = difference()(basic_geometry, Hexagon1)
+                        core = difference()(core, Hexagon1)
+        outside= import_('Outside.dxf')
+        trucks= import_('Trucks.dxf')
+        basic_geometry = core + outside + trucks
         scad_render_to_file(basic_geometry, file_out, file_header='$fn = %s;'%SEGMENTS)
-        export_name = "board"
         filenum = 0
         filename = "board" + str(filenum) + ".dxf"
         #figure out what to name generated file
@@ -35,4 +48,4 @@ def hexdraw(insize,outsize):
         os.system(commandlinearg)
         return filename
 
-hexdraw(20,50)
+hexdraw(20,30)
