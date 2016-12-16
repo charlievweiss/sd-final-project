29f3c4621399f444c008bc63315dbc579766f3c1
Andrew Holmes <andrew.holmes@students.olin.edu>
2016-12-16 01:58:21 -0500

    Cleaned main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-"""main page for app
+"""Main page for app
 - assigns upload folder for DXF
 - takes inputs from user by accessing index.html
 - calls board_math.py and hexdraw.py to calculate specs and create DXF
@@ -8,56 +8,49 @@
 from flask import Flask,render_template,request, redirect, url_for, send_from_directory
 import board_math
 import hexdraw
-from werkzeug.utils import secure_filename
-import os, sys
-from solid import *
-from solid.utils import *
-import math
 
 
 app = Flask(__name__)
 # configure folder for upload
 app.config['UPLOAD_FOLDER'] = 'uploads/'
-app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'DXF'])
+app.config['ALLOWED_EXTENSIONS'] = set(['DXF']) #List of allowed extensions. We only want DXF files.
 # present main page
 @app.route('/')
-def index():
+def index(): #Uses flask to render the homepage
 	return render_template('index.html')
 
-@app.route('/draw')
-def left_bar():
-	return render_template('draw.html')
+@app.route('/form')
+def left_bar(): #Renders the form and routes it to '/form'
+	return render_template('form.html')
 
-# present download page
+"""
+Takes in user input from form.html, checks that it is a valid entry, 
+generates a model based on the information, and sends the user to the download 
+page if valid.
+"""
 @app.route('/download',methods=['POST'])
 def download():
-	# accept user inputs from main_page.html (which points here)
 	rider_weight=request.form['rider_weight']
 	riding_style=request.form['riding_style']
 	if rider_weight and riding_style:
-			try:
-				#check for too high rider weights
-				if float(rider_weight)<=900:
+			try: #tests to see if the input returns an error of any kind and if so defaults to except
+				if float(rider_weight)<=900 and float(rider_weight) >= 20:
 					# Call board_math.py to get board parameters
-					# Returns list of small circle diameter, big circle diameter
-					rider_weight = int(rider_weight) #TODO: check if can make original inputs ints
+					rider_weight = int(rider_weight) 
 					riding_style = int(riding_style)
-					Outputs = board_math.board_math(rider_weight,riding_style)
-					# Call hexdraw.py to generate DXF
-					filename = hexdraw.hexdraw(Outputs[0],Outputs[1])
-					# Make filename access file at this location -- see below @app.route('/uploads/<filename>')
-					filename = 'http://127.0.0.1:5000/static/' + filename
-					return render_template('download.html', filename=filename)
+					Outputs = board_math.board_math(rider_weight,riding_style) #Returns list of small circle diameter, big circle diameter
+					filename ='http://127.0.0.1:5000/static/' + hexdraw.hexdraw(Outputs[0],Outputs[1]) # Call hexdraw.py to generate DXF and modify the filename to refer to the local window
+					return render_template('download.html', filename=filename) #render the download page, which references filename
 				else:
-					return render_template('error.html')
+					return render_template('error.html') #returns the error page if the weight is too large or too small
 			except:
-				return render_template('error.html')
+				return render_template('error.html') #returns the error page if any other issue occurs (ex. if the user enters "cat")
 	else:
-		return render_template('error.html')
+		return render_template('error.html') #returns the error page if the user does not fill out the form in its entirety
 
 # upload DXF file
-@app.route('/uploads/<filename>')
+@app.route('/uploads/<filename>') #Allows file to be uploaded and called for download on the web.
 def send_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-app.run(threaded=True)
+app.run(threaded=True) #Runs the app
